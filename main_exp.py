b277d01947561880c165c850c7783b20c87af95b
morrita11 <bob112955@gmail.com>
2026-01-05 13:11:31 -0500

    added stats_exp.py

--- main_exp.py
+++ main_exp.py
@@ -1,29 +1,36 @@
-# exempal of book bot for boots for helping with code 
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"Found {num_words} total words")
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
- # exempal of the reading code 
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-# exempel of the number of words code 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
-# form odd lesson 
-def count_vowels(text):
-    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-    count = 0
-    unique = set()
-    for char in text:
-        if char in vowels:
-            count += 1
-            unique.add(char)
-    return count, unique
